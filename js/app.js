de9e8302a8295f2d4dcb031ea61bc66ff1162f67
Victor Strand <92252033+Vss86@users.noreply.github.com>
2022-11-29 08:57:26 +0100

    Changed Projects

--- js/app.js
+++ js/app.js
@@ -14,7 +14,21 @@ async function getmyList() {
     for (let)
 
 
-  array.forEach(element => {
-	}
-});
+  }
+}
+
+var modal = document.getElementById(".img-slide");
+
+var li = document.getElementById(".slide");
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
