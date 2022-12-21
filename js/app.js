793ea247470d147f3bef2b55441d08e6d5539276
Victor Strand <92252033+Vss86@users.noreply.github.com>
2022-12-21 09:57:49 +0100

    Readme

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ async function getmyList() {
   }
 }
 
-var modal = document.getElementById("myModal");
+/* var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
@@ -35,4 +35,4 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
-};
+}; */
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://vss86.github.io/CV1/
