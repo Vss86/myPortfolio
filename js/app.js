fcbbe785ebf3ecf48d2d6c54236b2235fd8c426a
Victor Strand <92252033+Vss86@users.noreply.github.com>
2022-12-21 11:10:08 +0100

    Fixed json

--- js/app.js
+++ js/app.js
@@ -1,20 +1,71 @@
-let url = "data.json";
-const myList = document.querySelector(".WorkExperience");
+// fetch
+const getInfo = async () => {
 
-async function getmyList() {
-  let response = await fetch(url);
-
-  if (response.ok) {
-    let data = response.json();
-    console.log(data);
-    console.log(data.tomyList[0].myList);
-    let myList = data.myList;
-    console.log(typeof data);
+  const request = new Request('/data.json');
+  //fetch() starts a request and returns a promise. When the request completes, the promise is resolved with the Response object. 
+  //If the request fails due to some network problems, the promise is rejected.
 
 
-
-  }
+  const response = await fetch(request);
+ // await fetch('/cvjson') starts an HTTP request to '/movies' URL. Because the await keyword is present, 
+ //the asynchronous function is paused until the request completes.
+  const cvObj = await response.json();
+  //response.json() is a method on the Response object that lets you extract a JSON object from the response. 
+  //The method returns a promise, so you have to wait for the JSON: await response.json().
+  
+  //console.log(cvObj)
+  return cvObj;
+  
 }
+
+
+const populateCv = (cvObj) => {
+let employmentHtml = ``;
+
+cvObj.employment.forEach(element => {
+  const listItem = `
+  <li>
+  <h4>${element.time}</h4>
+  <p>
+  <span>${element.location}</span><br>
+  <span>${element.jobTitle}</span>
+
+  </p>
+  </li>`;
+
+  employmentHtml +=listItem
+  console.log(cvObj.employment);
+});
+document.getElementById('employmentList').innerHTML = employmentHtml;
+
+let educationHtml = ``;
+cvObj.education.forEach(element => {
+  const listItem = `<li>${element}</li>`;
+
+  educationHtml +=listItem
+  //console.log(cvObj.education);
+});
+
+document.getElementById('educationList').innerHTML = educationHtml;
+}
+
+/* let internshipsHtml = ``;
+cvObj.internships.forEach(element => {
+  let listItem = `<li>
+  <p>
+  <span >${element.companyName}</span>
+  <span>${element.text}</span><br>
+  </p>
+  </li>`;
+
+  internshipsHtml +=listItem
+  console.log(cvObj.internships);
+});
+document.getElementById('internshipsList').innerHTML = internshipsHtml;
+} */
+
+const cvObj = await getInfo();
+populateCv(cvObj);
 
 /* var modal = document.getElementById("myModal");
 
